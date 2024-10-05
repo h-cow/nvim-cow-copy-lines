8d8f9a9d3ecc0549dc77a1436c59ab41e4cfd074
Russell Murray <rm@mousezero.com>
2024-10-05 13:49:32 -0600

    Strip out over indentation

--- lua/nvim-cow-copy-lines/init.lua
+++ lua/nvim-cow-copy-lines/init.lua
@@ -1,3 +1,21 @@
+local HIGHEST_NUMBER = 9223372036854775807;
+
+function getMinIndent(lines)
+	local minIndent = HIGHEST_NUMBER
+	for k,v in ipairs(lines) do
+		local indent = string.match(v, "^%s*")
+		if indent == nil then
+			indent = ""
+		end
+		indent = string.len(indent)
+		if indent < minIndent then
+			minIndent = indent
+		end
+	end
+	return minIndent
+end
+
+
 function CowLineCopy(args)
 	if (args.line1 == nil or args.line2 == nil) then
 		print("You need to have text selected")
@@ -19,9 +37,11 @@ function CowLineCopy(args)
 	local lineRange = line_start .. "-" .. line_end
 	local fileType = vim.bo.filetype
 	local lines = vim.fn.getline(line_start, line_end)
+	local minIndent = getMinIndent(lines)
 	local code = ""
 	for k,v in ipairs(lines) do
-		code = code .. v .. "\n"
+		local currentLine = string.sub(v, minIndent + 1)
+		code = code .. currentLine .. "\n"
 	end
 
 	local output = table.concat({
